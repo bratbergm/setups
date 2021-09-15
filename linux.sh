9d68a8591d116a574832b98268bc2d9a2e87e514
morterb <morterb@stud.ntnu.no>
2021-09-15 16:51:51 +0200

    Linux setup

---
+++ linux.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install zsh
+
+sudo apt install powerline fonts-powerline
+
+# oh my zsh
+$ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
